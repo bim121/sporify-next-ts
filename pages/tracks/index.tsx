4c267d896a69822b49544611544efa41e1ab0cf8
bim121 <118322970+bim121@users.noreply.github.com>
2023-02-23 20:46:38 +0300

    first

---
+++ pages/tracks/index.tsx
@@ -0,0 +1,14 @@
+import NavBar from "@/component/NavBar";
+import React from "react";
+
+const Index = () => {
+    return(
+        <>
+            <div>
+                Срисок треков
+            </div>
+        </>
+    );
+};
+
+export default Index;
