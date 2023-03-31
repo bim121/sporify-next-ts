0f6d8cd4f8727472608f3fe2e35cfdf3ca4af32d
bim121 <118322970+bim121@users.noreply.github.com>
2023-04-01 01:06:31 +0300

    comment

--- pages/tracks/[id].tsx
+++ pages/tracks/[id].tsx
@@ -1,15 +1,35 @@
+import { useInput } from '@/hooks/useInput';
 import MainLayout from '@/layouts/MainLayout';
 import { ITrack } from '@/types/track';
 import { CommentsDisabled } from '@mui/icons-material';
 import { Button, Grid, TextField } from '@mui/material';
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 
-const TrackPage = () => {
-    const track: ITrack = {_id: "1", name: "name1", artist: "artist1", text:"text1", listens: 5, audio: "http://localhost:5001/audio/0473512f-665f-4f85-a58e-b1f67b432407.mp3", 
-        picture: "http://localhost:5001/image/c2f32f00-623d-49f4-85aa-89af9feecad6.jpg", comments: []}
+const TrackPage = ({serverTrack}) => {
+   const [track, setTrack] = useState(serverTrack); 
+    //const track: ITrack = {_id: "1", name: "name1", artist: "artist1", text:"text1", listens: 5, audio: "http://localhost:5001/audio/0473512f-665f-4f85-a58e-b1f67b432407.mp3", 
+    //    picture: "http://localhost:5001/image/c2f32f00-623d-49f4-85aa-89af9feecad6.jpg", comments: []}
 
     const router = useRouter();
+    const username = useInput('');
+    const text = useInput('');
+
+    const addComment = async () => {
+        try{
+                const response = await axios.post('http://localhost:5001/tracks/comment', {
+                    username: username.value,
+                    text: text.value,
+                    trackId: track._id
+                })
+            setTrack({...track, comments: [...track.comments, response.data]})
+        }catch(e){
+            console.log(e);
+        }
+        
+    }
 
     return(
         <div>
@@ -21,7 +41,7 @@ const TrackPage = () => {
                     К списку
                 </Button>
                 <Grid container style={{margin: '20px 0'}}>
-                    <img src={track.picture} width={200} alt="#"/>
+                    <img src={"http://localhost:5001/" + track.picture} width={200} alt="#"/>
                     <div style={{marginLeft: 30}}>
                         <h1>Название трека - {track.name}</h1>
                         <h1>Исполнитель - {track.artist}</h1>
@@ -35,14 +55,16 @@ const TrackPage = () => {
                     <TextField
                         label="Ваше имя"
                         fullWidth
+                        {...username}
                     />
                     <TextField
                         label="Комментарий"
                         fullWidth
                         multiline
                         rows={4}
+                        {...text}
                     />
-                    <Button>Отправить</Button>
+                    <Button onClick={addComment}>Отправить</Button>
                 </Grid>
                 <div>
                     {track.comments.map(comment =>
@@ -58,3 +80,13 @@ const TrackPage = () => {
 }
 
 export default TrackPage;
+
+export const getServerSideProps: GetServerSideProps = async({params}) => {
+    const url = 'http://localhost:5001/tracks/' + params?.id;
+    const response = await axios.get(url)
+    return {
+        props: {
+            serverTrack: response.data
+        }
+    }
+}
--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -1,17 +1,34 @@
 import FileUpload from '@/component/FileUpload';
 import StepWrapper from '@/component/StepWrapper';
+import { useInput } from '@/hooks/useInput';
 import MainLayout from '@/layouts/MainLayout';
 import { Button, Grid, TextField } from '@mui/material';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 
 const Create = () => {
     const [activeStep, setActiveStep] = useState(0);
     const [picture, setPicture] = useState(null);
     const [audio, setAudio] = useState(null);
+    const name = useInput('');
+    const artist = useInput('');
+    const text = useInput('');
+    const router = useRouter();
 
     const next = () => {
         if(activeStep !== 2){
             setActiveStep(prev => prev + 1);
+        } else{
+            const formData = new FormData()
+            formData.append('name', name.value)
+            formData.append('text', text.value)
+            formData.append('artist', artist.value)
+            formData.append('picture', picture)
+            formData.append('audio', audio)
+            axios.post('http://localhost:5001/tracks', formData)
+                .then(resp => router.push('/tracks'))
+                .catch(e => console.log(e))
         }
     }
     const back = () => {
@@ -25,14 +42,17 @@ const Create = () => {
                    {activeStep === 0 && 
                         <Grid container direction={'column'} style={{padding: 20}}>
                             <TextField
+                                {...name}
                                 style={{marginTop: 10}}
                                 label={"Название трека"}
                             />
                             <TextField
+                                {...artist}
                                 style={{marginTop: 10}}
                                 label={"Имя исполнителя"}
                             />
                             <TextField
+                                {...text}
                                 style={{marginTop: 10}}
                                 label={"Слова к треку"}
                                 multiline
